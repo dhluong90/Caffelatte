1f3e94ffc36dccc2b1a911a9c4f3d013a03462c4
Phong Pham Nguyen Quang <phamphongbsn@outlook.com>
2019-10-29 22:46:51 +0700

    sync configuration from prod

--- config/database.php
+++ config/database.php
@@ -64,11 +64,10 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'cafelatte'),
-            'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', 'postgres'),
+            'host'     => $herokuDb['host'],
+            'database' => substr($herokuDb['path'], 1),
+            'username' => $herokuDb['user'],
+            'password' => $herokuDb['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
